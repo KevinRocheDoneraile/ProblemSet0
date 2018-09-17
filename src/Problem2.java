bb09c3601834b7bc70cc1ccf19995a07f79f47d4
KevinRocheDoneraile <Kevin.Roche2@students.ittralee.ie>
2018-09-17 17:43:22 +0100

    coommit

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,4 +1,16 @@
-
+import javax.swing.*;
 
 public class Problem2 {
+    public static void main(String[] args) {
+        String firstname, initial, lastname;
+        int distance;
+        float money;
+
+        firstname = JOptionPane.showInputDialog("Enter your first name");
+        lastname = JOptionPane.showInputDialog("Enter your last name");
+        initial = JOptionPane.showInputDialog("Enter your initial");
+        distance = Integer.parseInt(JOptionPane.showInputDialog("Enter how long you cycle"));
+
+        JOptionPane.showMessageDialog(null,"First name: " + firstname + "\nLast name: " + lastname + "\nInitial: " + initial + "\nDistance: " + distance,"info",JOptionPane.PLAIN_MESSAGE);
+    }
 }
